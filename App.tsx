636ba4804e1d370a48bbe0773d149fce555e0a4d
공정훈 <rhdtl78@gmail.com>
2020-05-04 03:43:46 +0900

    use data to change WeaterScreen's content

--- App.tsx
+++ App.tsx
@@ -7,13 +7,24 @@ const OPEN_WEATER_MAP_API_KEY = '88c6bb40c5fc3c1e010f0d2eb4047dfe';
 
 type Weather = {
   temperature: number | string;
-  name: string;
+  name:
+    | 'Rain'
+    | 'Clear'
+    | 'Thunderstrom'
+    | 'Clouds'
+    | 'Snow'
+    | 'Drizzle'
+    | 'Haze'
+    | 'Mist';
 };
 
 export default function App() {
   const [isLoaded, setLoadState] = useState(false);
   const [error, setErrorState] = useState<PositionError>();
-  const [weather, setWeather] = useState<Weather>();
+  const [weather, setWeather] = useState<Weather>({
+    temperature: 0,
+    name: 'Rain',
+  });
   useEffect(() => {
     navigator.geolocation.getCurrentPosition(
       (position) => {
@@ -26,24 +37,26 @@ export default function App() {
     return;
   }, []);
 
-  const getWeather = (lat: number, lon: number) => {
-    fetch(
+  const getWeather = async (lat: number, lon: number) => {
+    const response = await fetch(
       `http://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${OPEN_WEATER_MAP_API_KEY}`
-    )
-      .then((response) => response.json())
-      .then((res) => {
-        setWeather({
-          temperature: res.main.temp,
-          name: res.weather[0].main,
-        });
-        setLoadState(true);
-      });
+    );
+    const res = await response.json();
+    setWeather({
+      temperature: res.main.temp,
+      name: res.weather[0].main,
+    });
+    setLoadState(true);
   };
 
   return (
     <View style={styles.container}>
       <StatusBar hidden={true} />
-      {!isLoaded ? <LoadingScreen /> : <WeatherScreen />}
+      {!isLoaded ? (
+        <LoadingScreen />
+      ) : (
+        <WeatherScreen temp={weather.temperature} weatherName={weather.name} />
+      )}
       {error && <Text>{error.message}</Text>}
     </View>
   );
--- src/screens/WeatherScreen/WeatherScreen.tsx
+++ src/screens/WeatherScreen/WeatherScreen.tsx
@@ -1,20 +1,103 @@
-import { Ionicons } from '@expo/vector-icons';
+import { MaterialCommunityIcons } from '@expo/vector-icons';
 import { LinearGradient } from 'expo-linear-gradient';
 import React, { ReactElement } from 'react';
 import { Text, View } from 'react-native';
 import styles from './WeatherScreenStyles';
-interface Props {}
+interface Props {
+  temp: number | string;
+  weatherName:
+    | 'Rain'
+    | 'Clear'
+    | 'Thunderstrom'
+    | 'Clouds'
+    | 'Snow'
+    | 'Drizzle'
+    | 'Haze'
+    | 'Mist';
+}
 
-export default function WeatherScreen({}: Props): ReactElement {
+type WeatherCase = {
+  colors: string[];
+  title: string;
+  subTitle: string;
+  iconName: string;
+};
+
+type WeatherCases = Record<string, WeatherCase>;
+
+const WEATHER_CASES: WeatherCases = {
+  Rain: {
+    colors: ['#00C6FB', '#005BEA'],
+    title: 'Raining like a MF',
+    subTitle: 'For more info look outside',
+    iconName: 'weather-rainy',
+  },
+  Clear: {
+    colors: ['#FEF253', '#FF7300'],
+    title: 'Sunny as fuck',
+    subTitle: 'Go get your ass burnt',
+    iconName: 'weather-sunny',
+  },
+  Thunderstorm: {
+    colors: ['#00ECBC', '#00&ADF'],
+    title: 'Thunderstorm int the house',
+    subTitle: 'Actually outside of ther house',
+    iconName: 'weather-lightning',
+  },
+  Clouds: {
+    colors: ['#D7D2CC', '#304352'],
+    title: 'Clouds',
+    subTitle: 'I know, fucking boring',
+    iconName: 'weather-cloudy',
+  },
+  Snow: {
+    colors: ['#7DE2FC', '#B9B6E5'],
+    title: 'Cold as balls',
+    subTitle: 'Do you want to build a snowman? Fuck no.',
+    iconName: 'weather-snowy',
+  },
+  Drizzle: {
+    colors: ['#89F7FE', '#66A6FF'],
+    title: 'Drizzle',
+    subTitle: 'Is like rain, but gay',
+    iconName: 'weather-hail ',
+  },
+  Haze: {
+    colors: ['#89F7FE', '#66A6FF'],
+    title: 'Haze',
+    subTitle: "Don't know what that is.",
+    iconName: 'weather-hail',
+  },
+  Mist: {
+    colors: ['#89F7FE', '#66A6FF'],
+    title: 'Haze',
+    subTitle: "It's like you have no glasses.",
+    iconName: 'weather-fog',
+  },
+};
+
+export default function WeatherScreen({
+  temp,
+  weatherName,
+}: Props): ReactElement {
   return (
-    <LinearGradient colors={['#00C6FB', '#005BEA']} style={styles.container}>
+    <LinearGradient
+      colors={WEATHER_CASES[weatherName].colors}
+      style={styles.container}
+    >
       <View style={styles.upper}>
-        <Ionicons color="white" size={150} name="ios-rainy" />
-        <Text style={styles.temp}>35°</Text>
+        <MaterialCommunityIcons
+          color="white"
+          size={150}
+          name={WEATHER_CASES[weatherName].iconName}
+        />
+        <Text style={styles.temp}>{Math.floor(Number(temp) - 273.15)}°</Text>
       </View>
       <View style={styles.lower}>
-        <Text style={styles.title}>Raining like a MF</Text>
-        <Text style={styles.subtitle}>For more info look outside</Text>
+        <Text style={styles.title}>{WEATHER_CASES[weatherName].title}</Text>
+        <Text style={styles.subtitle}>
+          {WEATHER_CASES[weatherName].subTitle}
+        </Text>
       </View>
     </LinearGradient>
   );
